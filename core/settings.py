a21cc85aab6b5e07c2dc8134cb979485b965e56c
benxoa <benxoax@gmail.com>
2023-10-26 17:08:44 +0500

    save arezzo files

--- core/settings.py
+++ core/settings.py
@@ -124,3 +124,6 @@ MEDIA_ROOT = 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = BASE_DIR, 'static'
+STATIC_ROOT = BASE_DIR, 'staticfiles_build', 'static'
